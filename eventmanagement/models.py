6a820c1896c4aebd3542fd405dc3170ca9577989
Sreekanth S <sreekanthsanthosh17@gmail.com>
2024-02-26 19:50:39 +0530

    added a calender

--- eventmanagement/models.py
+++ eventmanagement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    startdate = models.DateTimeField(null=True, blank=True)
+    enddate = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='event_images/', blank=True, null=True, default='default.jpg')
+
+    def Meta(self):
+        db_table = 'tblevents'
+    def __str__(self):
+        return self.name
--- eventmanagement/views.py
+++ eventmanagement/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def events(request):
-    return render(request, 'events.html')
+    if request.user.is_authenticated:
+        username = request.user.username
+        profile_image = request.user.profile.image.url
+        name = request.user.first_name + ' ' + request.user.last_name
+        return render(request, 'events.html', { 'username': username, 'profile_image': profile_image, 'name': name})
+    else:
+        messages.success(request, "You must be logged in to view your events.")
+        return redirect('index')
